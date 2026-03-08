27505fdeefbaf08723e9115172ea50bf0f99519e
Abir Hasan <abirhasan5208+github@gmail.com>
2026-03-08 15:12:18 +0600

    Implement stack using array

---
+++ src/stack/MyStack.java
@@ -0,0 +1,34 @@
+package stack;
+
+public class MyStack {
+
+    private int stackPointer = -1;
+    private final int[] elements = new int[10];
+
+    public int top() {
+        if (isEmpty()) {
+            throw new RuntimeException("Stack empty");
+        }
+        return elements[stackPointer];
+    }
+
+    public void push(int n) {
+        stackPointer++;
+        elements[stackPointer] = n;
+    }
+
+    public void pop() {
+        if (isEmpty()) {
+            throw new RuntimeException("Stack empty");
+        }
+        stackPointer--;
+    }
+
+    public boolean isEmpty() {
+        return stackPointer == -1;
+    }
+
+    public int getSize() {
+        return stackPointer + 1;
+    }
+}
